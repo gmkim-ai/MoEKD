80895a1de2f5e44b34d198ccdbef52386e51fc02
gmkim-ai <gmkim@kaist.ac.kr>
2024-08-28 22:11:31 +0900

    Implementing

--- llama_moe/modeling_llama_moe_hf.py
+++ llama_moe/modeling_llama_moe_hf.py
@@ -433,6 +433,9 @@ class TopKBalancedNoisyGate(nn.Module):
             self.mean = 0.0
             self.std = 1.0
             self.normal = Normal(self.mean, self.std)
+            device = torch.cuda.current_device()
+            self.normal.loc = self.normal.loc.to(device)
+            self.normal.scale = self.normal.scale.to(device)
             self.softplus = nn.Softplus()
 
         self.reset_parameters()
@@ -512,10 +515,6 @@ class TopKBalancedNoisyGate(nn.Module):
                 threshold_positions_if_out = threshold_positions_if_in - 1
                 threshold_if_out = torch.unsqueeze(torch.gather(top_values_flat, 0, threshold_positions_if_out), 1)
                 # is each value currently in the top k.
-                import torch.distributed as dist
-                if dist.get_rank() == 0:
-                    import pdb
-                    pdb.set_trace()
                 prob_if_in = self.normal.cdf((logits_gate - threshold_if_in) / noise_control)
                 prob_if_out = self.normal.cdf((logits_gate - threshold_if_out) / noise_control)
                 prob = torch.where(is_in, prob_if_in, prob_if_out)
