dbae2a1f9af30ff87d46549f8560e873921d755c
gmkim-ai <gmkim@kaist.ac.kr>
2024-09-03 16:47:18 +0900

    Implementing

---
+++ scripts/moe/sft/sft_3_0B-2_16_old.sh
@@ -0,0 +1,98 @@
+#! /bin/bash
+
+MASTER_ADDR=localhost
+MASTER_PORT=${2-2012}
+NNODES=1
+NODE_RANK=0
+GPUS_PER_NODE=${3-2}
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE \
+                  --nnodes $NNODES \
+                  --node_rank $NODE_RANK \
+                  --master_addr $MASTER_ADDR \
+                  --master_port $MASTER_PORT"
+
+# model
+BASE_PATH=${1-"."}
+CKPT_NAME="3_0B-2_16"
+CKPT="${BASE_PATH}/checkpoints/llama-moe/foundation/${CKPT_NAME}"
+# CKPT="huggyllama/llama-7b"
+# data
+DATA_DIR="${BASE_PATH}/processed_data/dolly/full/moe/"
+# hp
+BATCH_SIZE=1
+LR=0.00005
+MIN_LR=0.000005
+GRAD_ACC=8
+EVAL_BATCH_SIZE=16
+# length
+MAX_LENGTH=512
+# runtime
+SAVE_PATH="${BASE_PATH}/results/moe/train/sft/sft_3_0B-2_16"
+# seed
+SEED=10
+SEED_ORDER=10
+
+
+OPTS=""
+# model
+OPTS+=" --base-path ${BASE_PATH}"
+OPTS+=" --model-path ${CKPT}"
+OPTS+=" --ckpt-name ${CKPT_NAME}"
+OPTS+=" --n-gpu ${GPUS_PER_NODE}"
+OPTS+=" --model-type moe"
+# OPTS+=" --gradient-checkpointing"
+# data
+OPTS+=" --data-dir ${DATA_DIR}"
+OPTS+=" --num-workers 8"
+OPTS+=" --dev-num -1"
+# hp
+OPTS+=" --lr ${LR}"
+OPTS+=" --lr-min ${MIN_LR}" #EDIT
+OPTS+=" --batch-size ${BATCH_SIZE}"
+OPTS+=" --eval-batch-size ${EVAL_BATCH_SIZE}"
+OPTS+=" --gradient-accumulation-steps ${GRAD_ACC}"
+OPTS+=" --warmup-iters 0"
+OPTS+=" --lr-decay-style cosine"
+OPTS+=" --weight-decay 0.1" #1e-2 EDIT
+OPTS+=" --clip-grad 1.0" #1.0
+OPTS+=" --epochs 10"
+# length
+OPTS+=" --max-length ${MAX_LENGTH}"
+OPTS+=" --max-prompt-length 256"
+# runtime
+OPTS+=" --do-train"
+OPTS+=" --do-valid"
+OPTS+=" --eval-gen"
+OPTS+=" --save-interval -1"
+OPTS+=" --eval-interval -1"
+OPTS+=" --log-interval 10"
+OPTS+=" --mid-log-num -1"
+OPTS+=" --save ${SAVE_PATH}"
+# seed
+OPTS+=" --seed ${SEED}"
+OPTS+=" --seed-order ${SEED_ORDER}"
+# # deepspeed
+OPTS+=" --deepspeed"
+OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config.json"
+# type
+OPTS+=" --type lm"
+# gen
+OPTS+=" --do-sample"
+OPTS+=" --top-k 0"
+OPTS+=" --top-p 1.0"
+OPTS+=" --temperature 1.0"
+
+
+export NCCL_DEBUG=""
+export WANDB_DISABLED=True
+export TF_CPP_MIN_LOG_LEVEL=3
+export PYTHONPATH=${BASE_PATH}
+export PT_HPU_LAZY_MODE=0
+# OPTS+=" --use_lazy_mode=False"
+CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/finetune.py ${OPTS} $@"
+
+echo ${CMD}
+echo "PYTHONPATH=${PYTHONPATH}"
+mkdir -p ${SAVE_PATH}
+${CMD}
