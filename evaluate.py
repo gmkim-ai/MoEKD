5b76097ac6b3aea98084367ad54b492ef9221c23
gmkim-ai <gmkim@kaist.ac.kr>
2024-09-07 00:18:14 +0900

    Implementing

--- evaluate.py
+++ evaluate.py
@@ -54,6 +54,12 @@ def main():
     #torch.backends.cudnn.enabled = False
     
     args = get_args()
+
+    # if arges.save folder exists, then terminate
+    if os.path.exists(args.save):
+        print(f"Folder {args.save} exists. Terminating.")
+        return
+
     initialize(args)
     
     if dist.get_rank() == 0:
--- scripts/moe/eval/run_eval_.sh
+++ scripts/moe/eval/run_eval_.sh
@@ -8,7 +8,7 @@ for data in dolly self_inst vicuna sinst uinst
 do
     for seed in 10 20 30 40 50
     do
-        bash ${base_path}/scripts/moe/eval/eval_main_${data}.sh ${base_path} ${port} 2 ${ckpt} --model-type ${model_type} --seed $seed  --eval-batch-size 32 --save "${base_path}/results/moe/eval_main_/"
+        bash ${base_path}/scripts/moe/eval/eval_main_${data}.sh ${base_path} ${port} 2 ${ckpt} --model-type ${model_type} --seed $seed  --eval-batch-size 32
         sleep 10
     done
 done
