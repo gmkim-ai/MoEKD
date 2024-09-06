419998176b5e0c4061de4de4a15b5088b82c7c9b
gmkim-ai <gmkim@kaist.ac.kr>
2024-09-07 00:09:37 +0900

    Implementing

---
+++ scripts/moe/eval/run_eval_.sh
@@ -0,0 +1,14 @@
+base_path=${1-"."}
+ckpt=${2-"."}
+port=${3-"15031"}
+model_type=${4-"llama"}
+
+
+for data in dolly self_inst vicuna sinst uinst 
+do
+    for seed in 10 20 30 40 50
+    do
+        bash ${base_path}/scripts/moe/eval/eval_main_${data}.sh ${base_path} ${port} 2 ${ckpt} --model-type ${model_type} --seed $seed  --eval-batch-size 32 --save "${base_path}/results/moe/eval_main_/"
+        sleep 10
+    done
+done
