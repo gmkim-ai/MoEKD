963ca5d2778c47b5574226eb7cc140168507f793
gmkim-ai <gmkim@kaist.ac.kr>
2024-09-09 21:12:06 +0900

    Implementing

--- scripts/moe/eval/run_eval.sh
+++ scripts/moe/eval/run_eval.sh
@@ -2,13 +2,14 @@ base_path=${1-"."}
 ckpt=${2-"."}
 port=${3-"15031"}
 model_type=${4-"llama"}
+gpu_num=${5-"1"}
 
 
 for data in dolly self_inst vicuna sinst uinst 
 do
     for seed in 10 20 30 40 50
     do
-        bash ${base_path}/scripts/moe/eval/eval_main_${data}.sh ${base_path} ${port} 2 ${ckpt} --model-type ${model_type} --seed $seed  --eval-batch-size 32
+        bash ${base_path}/scripts/moe/eval/eval_main_${data}.sh ${base_path} ${port} ${gpu_num} ${ckpt} --model-type ${model_type} --seed $seed  --eval-batch-size 32
         sleep 10
     done
 done
--- scripts/moe/sft/sft_3_5B-4_16_try.sh
+++ scripts/moe/sft/sft_3_5B-4_16_try.sh
@@ -4,7 +4,7 @@ MASTER_ADDR=localhost
 MASTER_PORT=${2-2012}
 NNODES=1
 NODE_RANK=0
-GPUS_PER_NODE=${3-2}
+GPUS_PER_NODE=${3-4}
 
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE \
                   --nnodes $NNODES \
@@ -14,56 +14,50 @@ DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE \
 
 # model
 BASE_PATH=${1-"."}
-CKPT_NAME="sft_init_1_3B"
-CKPT="${BASE_PATH}/results/moe/train/sft/sft_1_3B/e10-bs8-lr1e-05-G1-N2-NN1/sft_init"
+CKPT_NAME="3_5B-4_16"
+CKPT="${BASE_PATH}/checkpoints/llama-moe/foundation/${CKPT_NAME}"
+#CKPT="${BASE_PATH}/results/moe/train/sft/sft_3_0B-2_16/e10-bs1-lr5e-05-G8-N2-NN1-old-epoch8/best_rougeL"
 # CKPT="huggyllama/llama-7b"
-TEACHER_CKPT_NAME="2_7B"
-TEACHER_CKPT="${BASE_PATH}/results/moe/train/sft/sft_2_7B/e10-bs8-lr1e-05-G1-N2-NN1/best_rougeL"
-# MP_SIZE=4
 # data
 DATA_DIR="${BASE_PATH}/processed_data/dolly/full/moe/"
 # hp
-BATCH_SIZE=8
-LR=0.00001
+BATCH_SIZE=4
+LR=0.00001 #0.0001
+#MIN_LR=0.00001
 GRAD_ACC=1
 EVAL_BATCH_SIZE=32
 # length
 MAX_LENGTH=512
 # runtime
-SAVE_PATH="${BASE_PATH}/results/moe/train/kd/kd_2_7B_to_1_3B"
+SAVE_PATH="${BASE_PATH}/results/moe/train/sft/sft_3_5B-4_16"
 # seed
 SEED=10
+SEED_ORDER=10
 
 
 OPTS=""
 # model
 OPTS+=" --base-path ${BASE_PATH}"
 OPTS+=" --model-path ${CKPT}"
-OPTS+=" --teacher-model-path ${TEACHER_CKPT}"
 OPTS+=" --ckpt-name ${CKPT_NAME}"
-OPTS+=" --teacher-ckpt-name ${TEACHER_CKPT_NAME}"
-OPTS+=" --teacher-model-type llama"
-OPTS+=" --teacher-model-fp16"
 OPTS+=" --n-gpu ${GPUS_PER_NODE}"
-OPTS+=" --model-type llama"
+OPTS+=" --model-type moe"
 # OPTS+=" --gradient-checkpointing"
-# OPTS+=" --model-parallel"
-# OPTS+=" --model-parallel-size ${MP_SIZE}"
 # data
 OPTS+=" --data-dir ${DATA_DIR}"
-OPTS+=" --num-workers 4"
+OPTS+=" --num-workers 8"
 OPTS+=" --dev-num -1"
 # hp
 OPTS+=" --lr ${LR}"
+#OPTS+=" --lr-min ${MIN_LR}" #EDIT
 OPTS+=" --batch-size ${BATCH_SIZE}"
 OPTS+=" --eval-batch-size ${EVAL_BATCH_SIZE}"
 OPTS+=" --gradient-accumulation-steps ${GRAD_ACC}"
 OPTS+=" --warmup-iters 0"
 OPTS+=" --lr-decay-style cosine"
-OPTS+=" --weight-decay 1e-2"
-OPTS+=" --clip-grad 1.0"
+OPTS+=" --weight-decay 0.1" #1e-2 EDIT
+OPTS+=" --clip-grad 1.0" #1.0
 OPTS+=" --epochs 10"
-OPTS+=" --kd-ratio 0.5"
 # length
 OPTS+=" --max-length ${MAX_LENGTH}"
 OPTS+=" --max-prompt-length 256"
@@ -78,11 +72,12 @@ OPTS+=" --mid-log-num -1"
 OPTS+=" --save ${SAVE_PATH}"
 # seed
 OPTS+=" --seed ${SEED}"
-# deepspeed
+OPTS+=" --seed-order ${SEED_ORDER}"
+# # deepspeed
 OPTS+=" --deepspeed"
 OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config.json"
 # type
-OPTS+=" --type kd"
+OPTS+=" --type lm"
 # gen
 OPTS+=" --do-sample"
 OPTS+=" --top-k 0"
@@ -96,9 +91,12 @@ export TF_CPP_MIN_LOG_LEVEL=3
 export PYTHONPATH=${BASE_PATH}
 export PT_HPU_LAZY_MODE=0
 export OMP_NUM_THREADS=8
+# OPTS+=" --use_lazy_mode=False"
 CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/finetune.py ${OPTS} $@"
 
 echo ${CMD}
 echo "PYTHONPATH=${PYTHONPATH}"
 mkdir -p ${SAVE_PATH}
 ${CMD}
+
+bash scripts/moe/eval/run_eval.sh . results/moe/train/sft/sft_3_5B-4_16/e10-bs4-lr1e-05-G1-N4-NN1/best_rougeL 15035 moe ${GPUS_PER_NODE}
