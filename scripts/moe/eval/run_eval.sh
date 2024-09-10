f399f7d9cb876bf6217eea2f8e16cac60a74a1bc
gmkim-ai <gmkim@kaist.ac.kr>
2024-09-10 22:17:37 +0900

    Implementing

--- scripts/moe/eval/run_eval.sh
+++ scripts/moe/eval/run_eval.sh
@@ -4,11 +4,23 @@ port=${3-"15031"}
 model_type=${4-"llama"}
 gpu_num=${5-"1"}
 
-for data in dolly self_inst vicuna sinst_11_ uinst_11_
+for data in dolly self_inst vicuna
 do
     for seed in 10 20 30 40 50
     do
         while ! test -f ./results/moe/eval_main/${data}-512/${ckpt//"/"/"_"}/${seed}/preds.txt
+        do
+            bash ${base_path}/scripts/moe/eval/eval_main_${data}.sh ${base_path} ${port} ${gpu_num} ${ckpt} --model-type ${model_type} --seed $seed  --eval-batch-size 64
+            sleep 20
+        done
+    done
+done
+
+for data in sinst uinst
+do
+    for seed in 10 20 30 40 50
+    do
+        while ! test -f ./results/moe/eval_main/${data}_11_-512/${ckpt//"/"/"_"}/${seed}/preds.txt
         do
             bash ${base_path}/scripts/moe/eval/eval_main_${data}.sh ${base_path} ${port} ${gpu_num} ${ckpt} --model-type ${model_type} --seed $seed  --eval-batch-size 64
             sleep 20
