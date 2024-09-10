6e5f2c3213d35ed9acc357be97300ab1ae59b6af
gmkim-ai <gmkim@kaist.ac.kr>
2024-09-10 12:21:48 +0900

    Implementing

--- scripts/moe/eval/run_eval.sh
+++ scripts/moe/eval/run_eval.sh
@@ -14,4 +14,13 @@ do
     done
 done
 
+for data in dolly self_inst vicuna sinst uinst 
+do
+    for seed in 10 20 30 40 50
+    do
+        bash ${base_path}/scripts/moe/eval/eval_main_${data}.sh ${base_path} ${port} ${gpu_num} ${ckpt} --model-type ${model_type} --seed $seed  --eval-batch-size 32
+        sleep 10
+    done
+done
+
 python3 compute_score.py --name ${ckpt}
