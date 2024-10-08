90e23cf36d33ec3645170e20eded42dbfd15a42e
gmkim-ai <gmkim@kaist.ac.kr>
2024-10-08 23:05:58 +0900

    Implementing

--- llama_moe/modeling_llama_moe_hf.py
+++ llama_moe/modeling_llama_moe_hf.py
@@ -510,7 +510,6 @@ class TopKBalancedNoisyGate(nn.Module):
 
         top_logits, top_indices = logits.topk(min(self.num_selects + 1, self.num_experts), dim=1)  # 选择并排序前k+1个权重 -> english: Select and sort the top k+1 weights
         top_k_logits = top_logits[:, :self.num_selects]
-        print("Top K logits Shape: ", top_k_logits.shape)
         top_k_indices = top_indices[:, :self.num_selects]
         top_k_scores = self.softmax(top_k_logits.to(torch.float32)) if self.use_softmax else top_k_logits
         top_k_scores = top_k_scores.to(logits.dtype)
