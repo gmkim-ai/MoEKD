f87eceeb6d7b769e869f905f0d48e74aa8872d19
gmkim-ai <gmkim@kaist.ac.kr>
2024-11-16 23:34:53 +0900

    Implementing

--- scripts/moe/sfrmoekd/kd_3_5B-4_16_to_1_3B_sfr.sh
+++ scripts/moe/sfrmoekd/kd_3_5B-4_16_to_1_3B_sfr.sh
@@ -24,7 +24,7 @@ TEACHER_CKPT="${BASE_PATH}/results/moe/train/sft/sft_3_5B-4_16/e10-bs4-lr1e-05-G
 DATA_DIR="${BASE_PATH}/processed_data/dolly/full/moe/"
 # hp
 BATCH_SIZE=4
-LR=1e-05
+LR=5e-06
 GRAD_ACC=1
 EVAL_BATCH_SIZE=32
 # length
@@ -36,7 +36,7 @@ SEED=10
 
 # MoE KD
 NUM_SELECTS=16
-TEACHER_LR=1e-05
+TEACHER_LR=5e-06
 
 OPTS=""
 # moekd
@@ -107,10 +107,10 @@ CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/finetune_sfr.py ${OPTS} $@"
 echo ${CMD}
 echo "PYTHONPATH=${PYTHONPATH}"
 mkdir -p ${SAVE_PATH}
-while ! test -f ./results/moe/train/sfrmoekd/moekd_1_3B/e10-bs4-lr1e-05-G1-N4-NN1-kd0.5-topk${NUM_SELECTS}-tlr${TEACHER_LR}/best_rougeL/log.txt
+while ! test -f ./results/moe/train/sfrmoekd/moekd_1_3B/e10-bs4-lr5e-06-G1-N4-NN1-kd0.5-topk${NUM_SELECTS}-tlr${TEACHER_LR}/best_rougeL/log.txt
 do
     ${CMD}
     sleep 20
 done
 
-#bash scripts/moe/eval/run_eval.sh . results/moe/train/sfrmoekd/moekd_1_3B/e10-bs4-lr1e-05-G1-N4-NN1-kd0.5-topk${NUM_SELECTS}-tlr${TEACHER_LR}/best_rougeL 15035 llama ${GPUS_PER_NODE}
+#bash scripts/moe/eval/run_eval.sh . results/moe/train/sfrmoekd/moekd_1_3B/e10-bs4-lr5e-06-G1-N4-NN1-kd0.5-topk${NUM_SELECTS}-tlr${TEACHER_LR}/best_rougeL 15035 llama ${GPUS_PER_NODE}
