1648c46a43d7a8e4af7340a6c49984d4cb0a206e
gmkim-ai <gmkim@kaist.ac.kr>
2025-01-20 20:16:14 +0900

    Implementing

--- scripts/moe/eval/run_eval.sh
+++ scripts/moe/eval/run_eval.sh
@@ -5,11 +5,17 @@ model_type=${4-"llama"}
 gpu_num=${5-"1"}
 num_selects=${6-"None"}
 
+if [ ${model_type} == "moe" ] && [ ${num_selects} != "None" ]; then
+    ckpt_path=${ckpt//"/"/"_"}_ns${num_selects}
+else
+    ckpt_path=${ckpt//"/"/"_"}
+fi
+
 for data in dolly self_inst vicuna
 do
     for seed in 10 20 30 40 50
     do
-        while ! test -f ./results/moe/eval_main/${data}-512/${ckpt//"/"/"_"}/${seed}/preds.txt
+        while ! test -f ./results/moe/eval_main/${data}-512/${ckpt_path}/${seed}/preds.txt
         do
             bash ${base_path}/scripts/moe/eval/eval_main_${data}.sh ${base_path} ${port} ${gpu_num} ${ckpt} ${num_selects} --model-type ${model_type} --seed $seed  --eval-batch-size 64
             sleep 20
@@ -21,7 +27,7 @@ for data in sinst uinst
 do
     for seed in 10 20 30 40 50
     do
-        while ! test -f ./results/moe/eval_main/${data}_11_-512/${ckpt//"/"/"_"}/${seed}/preds.txt
+        while ! test -f ./results/moe/eval_main/${data}_11_-512/${ckpt_path}/${seed}/preds.txt
         do
             bash ${base_path}/scripts/moe/eval/eval_main_${data}.sh ${base_path} ${port} ${gpu_num} ${ckpt} ${num_selects} --model-type ${model_type} --seed $seed  --eval-batch-size 64
             sleep 20
