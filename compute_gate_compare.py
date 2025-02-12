03250a5f7ed23714ab68068485fe5d52b6dafd86
gmkim-ai <gmkim@kaist.ac.kr>
2025-02-12 22:24:07 +0900

    Implementing

--- compute_gate_compare.py
+++ compute_gate_compare.py
@@ -28,6 +28,8 @@ def main():
 
     for layer_idx in range(layer_num):
         layer_kl_div = []
+        layer_top_logits_orig = []
+        layer_top_logits_sar = []
         for idx in range(len(gate_files_orig)):
             gate_logits_orig = torch.load(os.path.join(args.gate_orig, f"{idx+1}.pt"), map_location=torch.device('cpu'))
             gate_logits_sar = torch.load(os.path.join(args.gate_sar, f"{idx+1}.pt"), map_location=torch.device('cpu'))
@@ -36,25 +38,29 @@ def main():
             gate_logit_orig = gate_logits_orig[layer_idx]
             gate_logit_orig = F.softmax(gate_logit_orig.to(torch.float32), dim=1)
             gate_logit_sar = gate_logits_sar[layer_idx]
-            gate_logit_sar = F.log_softmax(gate_logit_sar.to(torch.float32), dim=1)
+            gate_logit_sar = F.softmax(gate_logit_sar.to(torch.float32), dim=1)
 
             #top_logits, top_indices = gate_logit.topk(gate_logit.shape[-1], dim=1)
             valid_top_logits_orig = gate_logit_orig[(label != -100).nonzero()].squeeze()  # (response_part_length, # experts)
             valid_top_logits_sar = gate_logit_sar[(label != -100).nonzero()].squeeze()
 
             # Compute the KL divergence between the two distributions
-            kl_div_loss = kl_div(valid_top_logits_sar, valid_top_logits_orig).sum(-1)
+            kl_div_loss = kl_div(valid_top_logits_sar.log(), valid_top_logits_orig).sum(-1)
             # stack valid_top_logits to layer_top_logits
             layer_kl_div.append(kl_div_loss)
+            layer_top_logits_orig.append(valid_top_logits_orig)
+            layer_top_logits_sar.append(valid_top_logits_sar)
           
         layer_kl_div = torch.cat(layer_kl_div, dim=0)
 
         mean_kl_loss = layer_kl_div.mean(0)
-        max_kl_loss = layer_kl_div.max(0)
-        min_kl_loss = layer_kl_div.min(0)
+        max_kl_loss = layer_kl_div.max(0).values
+        min_kl_loss = layer_kl_div.min(0).values
         print(f"Layer {layer_idx+1} mean KL divergence: {mean_kl_loss}")
         print(f"Layer {layer_idx+1} max KL divergence: {max_kl_loss}")
         print(f"Layer {layer_idx+1} min KL divergence: {min_kl_loss}")
+        print(f"Layer {layer_idx+1} max top logits orig: {layer_top_logits_orig[layer_kl_div.max(0).indices]}")
+        print(f"Layer {layer_idx+1} max top logits sar: {layer_top_logits_sar[layer_kl_div.max(0).indices]}")
         print("---------------------------------------------")
 
 if __name__ == "__main__":
